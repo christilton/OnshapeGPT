4f1aa664fe30c3ec414e9e9c8adb3ddc68035026
christilton <84406266+christilton@users.noreply.github.com>
2024-02-28 15:04:54 -0500

    Create reqstemplate.py

---
+++ reqstemplate.py
@@ -0,0 +1,7 @@
+#Replace the empty strings with API keys and a document URL
+
+os_api_key = '' #Onshape API Key
+api_secret = '' #Onshape Secret Key
+os_api_keys = (os_api_key, api_secret)
+open_api_key ='' #OpenAI API Key
+url = '' #Document URL for the Onshape Document that will recieve the geometry
